e03b9527c2a115220a6bc33589e408dbb928a3ac
Ruben E. Gonzalez <rubengonzalez31241@gmail.com>
2026-08-06 20:47:46 -0300

    Fija versiones de langchain y ajusta imports

--- app_web.py
+++ app_web.py
@@ -6,9 +6,9 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain_groq import ChatGroq
-from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
-from langchain.chains.retrieval import create_retrieval_chain
+from langchain.chains import create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
 
 st.set_page_config(page_title="Asistente RAG Comercial", layout="wide")
 st.title("🤖 Asistente Virtual Inteligente")
